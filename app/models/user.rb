26702c21c284a00e39425160fb34c6a940f7924b
EC2 Default User <ec2-user@ip-172-31-19-56.ap-southeast-2.compute.internal>
2023-10-20 12:05:23 +0000

    [Create]validate

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,7 @@ class User < ApplicationRecord
   has_one_attached :profile_image
   
   validates :name, presence: true
-  validates :introduction, presence: true
+  validates :introduction, presence: false
   
   def get_profile_image(width, height)
     unless profile_image.attached?
